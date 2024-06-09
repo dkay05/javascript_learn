48dbf39e4492dbc0ac317a31f4b86f6c47740761
dkay05 <dewangdkay2004@gmail.com>
2024-06-09 21:04:22 +0530

    Add files via upload

---
+++ 03_conditional/index.js
@@ -0,0 +1,56 @@
+// practice qution
+
+let age= 19;
+if(age>=18){
+    console.log("you Can rive")
+}
+else{
+    console.log(" you can't Drive")
+}
+
+
+
+
+
+
+// practice question 1
+
+let a = 23;
+if(a>=10 && a<=20)
+{
+    console.log("you are in 10 to 20")
+}
+else{
+    console.log("you not are in 10 to 20")
+}
+
+// practice question 2
+
+let num = 9;
+num1 = num % 2;
+num2 = num % 3;
+
+if(num1 == 0 || num2 == 0)
+    {
+        console.log(num,"divided either 2 and 3")
+    }
+else{
+        console.log(num,"not divided")
+}
+
+// practice question 2
+
+let n = 10;
+num1 = n % 2;
+num2 = n % 3;
+
+if(num1 == 0 && num2 == 0)
+    {
+        console.log(n,"divided by 2 and 3")
+    }
+else{
+        console.log(n,"not divided")
+}
+
+// practice question 3 
+
