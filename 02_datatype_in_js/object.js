b4daaaf4c9610f6c4a5d49fe816e7779f0cbf972
dkay05 <dewangdkay2004@gmail.com>
2024-06-09 13:37:42 +0530

    Add files via upload

---
+++ 02_datatype_in_js/object.js
@@ -0,0 +1,22 @@
+
+// primitive datatypes 
+let x = "dkay";
+let y = 20;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+
+// Non primitive object 
+
+let o = {
+    "name": "dewang",
+    "age": 20
+}
+console.log(o)
+o.sallary= "100cr";
+console.log(o)
+o.sallary="500cr"
+console.log(o)
